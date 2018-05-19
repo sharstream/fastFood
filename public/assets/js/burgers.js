cf00d62cd330d2732e9e6311fa456580a08b50b2
David Perez <david.perez@us.abb.com>
2018-05-18 23:16:52 -0400

    client end put and del methods

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -2,13 +2,36 @@
 
 $(document).ready(function(){
 
+    // event handler to update a burger
+
+    $('#change-devoured').on('click', function (event) {
+
+        var data_id = $(this).data('id');
+        var devoure = $(this).data('devoured');
+
+        var newBurger = {
+            devoured: devoure
+        };
+
+        console.log(data_id);
+
+        $.ajax("/api/burgers/" + data_id, {
+            type: 'PUT',
+            data: newBurger
+        }).then(function(err, result){
+            console.log("burger devoured!");
+            location.reload();
+        });
+    });
 
     //event handler to create a burger
 
     $(".create-form").on("submit", function (event) {
         event.preventDefault();
+
         console.log('burger name: ' + $("#bu").val().trim());
         console.log('devoured: ' + $("[name=devoured]:checked").val());
+
         var newBurger = {
             burger_name: $("#bu").val().trim(),
             devoured: $("[name=devoured]:checked").val()
@@ -27,5 +50,17 @@ $(document).ready(function(){
         );
     });
 
-    //event handler to update a burger
+    //event handler to delete a burger
+
+    $('#delete-burger').on('click', function (event) {
+
+        var delete_id = $(this).data('id');
+
+        $.ajax("/api/burgers/" + delete_id, {
+            type: "DELETE"
+        }).then(function(){
+            console.log("burger deleted!");
+            location.reload();
+        })
+    });
 });
