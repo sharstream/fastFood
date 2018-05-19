7e8c259728ad14cea5f2fa8acde1f52abc83be78
David Perez <david.perez@us.abb.com>
2018-05-19 00:15:34 -0400

    cosmetic fixes - key/value and del bugfix

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,17 @@
 // MVC models - object relational mapper
 var connection = require('./connection.js');
 
+var obj = {
+    devoured: "devoured",
+    "data type": typeof this,
+    burger_name: function () {
+        console.log(this.devoured);
+    },
+    devoured: function () {
+        console.log(this["data type"]);
+    }
+};
+
 function printQuestionMarks(num) {
     var arr = [];
 
@@ -19,7 +30,7 @@ var orm = {
         connection.query(queryString, [tableInput], (err, rows) => {
             if(err) throw err;
             console.log('Data received from Db:\n');
-            rows.forEach((row) => {
+            rows.forEach(row => {
                 console.log(`${row.burger_name} is ${row.devoured}`);
             });
             cb(rows);
@@ -44,8 +55,29 @@ var orm = {
         });
     },
     updateOne: (tableInput, objColVals, condition, cb) => {
-        var queryString = 'update ?? set ?? where ?? = ?';
-        connection.query(queryString, [tableInput, objColVals, condition], (err, row) => {
+        var queryString = 'update ' + tableInput;
+        // array like object
+        // var arrStr = Object.entries(objColVals);
+        var arr = [];
+        for (const key in objColVals) {
+            if (objColVals.hasOwnProperty(key)) {
+                const element = objColVals[key];
+                if (typeof element === 'string' && element.indexOf(' ') >= 0) {
+                    element = "'" + element + "'";
+                }
+                arr.push(key + '=' + element);
+            }
+        }
+        //then convert to string
+        console.log('array like object ' + arrStr);
+        console.log('convert to str ' + arrStr.toString());
+        queryString += ' set ';
+        queryString += arr;
+        queryString += ' where ';
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, (err, row) => {
             if (err) throw err;
             console.log('Data received from Db:\n');
             console.log(row);
@@ -53,9 +85,12 @@ var orm = {
         });
     },
     deleteOne: (tableInput, objColVals, condition, cb) => {
-        var queryString = 'delete ?? set ?? where ?? = ?';
+        var queryString = 'delete from ' + tableInput;
+        queryString += ' where ';
+        queryString += condition;
+
         console.log(queryString);
-        connection.query(queryString, [tableInput, objColVals, condition], (err, row) => {
+        connection.query(queryString, (err, row) => {
             if(err) throw err;
             console.log(row);
             cb(row);
